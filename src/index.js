d406ece0935203b3d6a44738e9501c5795317fb1
aneng <aneng@pinduoduo.com>
2019-08-27 17:56:43 +0800

    feat(project): test eslint

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { Hello } from "./demo";
 
 ReactDOM.render(
+    console.log('此为生产环境'),
     <Hello compiler="TypeScript" framework="React" />,
     document.getElementById("app")
 );
